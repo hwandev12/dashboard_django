6c4fcce0be63819930cb102c0780f1b97dc8ce53
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-16 16:42:29 +0500

    connect dashboard model

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,8 +1,4 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib import admin
+from .models import DashboardTopCards
 
-# Register your models here.
+admin.site.register(DashboardTopCards)
