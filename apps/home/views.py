83e06df6f2c6085605d853b80b1bdab02c6167b9
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-16 16:43:29 +0500

    create dashboard card model context

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import DashboardTopCards
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
+    cards = DashboardTopCards.objects.all()
+    context = {'segment': 'index', 'cards': cards}
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
@@ -16,8 +18,6 @@ def index(request):
 @login_required(login_url="/login/")
 def pages(request):
     context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
     try:
 
         load_template = request.path.split('/')[-1]
@@ -37,3 +37,4 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
