25b0585e150c9f8fde3dfd03c0e4a0febd64fbfd
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-20 21:55:26 +0500

    create home sliders model

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class HomeSliders(models.Model):
+    class Meta:
+        verbose_name = 'Home Slider'
+        verbose_name_plural = 'Home Sliders'
+
+    slider_text = models.CharField(max_length=101)
+
+    def __str__(self):
+        return self.slider_text
+
