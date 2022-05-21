ea40b999d59c46e16ef625c8ce49c34f1712d82d
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-21 22:39:49 +0500

    create phone cover context

--- apps/base/views.py
+++ apps/base/views.py
@@ -5,8 +5,10 @@ from .models import *
 def base_home(request):
     sliders = HomeSliders.objects.all()
     fashion_cards = FashionCards.objects.all()
+    phone_cover = PhoneBackCovers.objects.all()
     context = {
         "sliders": sliders,
-        "fashion_cards": fashion_cards
+        "fashion_cards": fashion_cards,
+        "phone_cover": phone_cover
     }
     return render(request, 'base/pages/home.html', context)
