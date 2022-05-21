e51200e370468e8aecf662e3e238bf35e9f1e090
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-21 21:29:40 +0500

    create fashion model

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 
+# create home sliders model
 class HomeSliders(models.Model):
+
     class Meta:
         verbose_name = 'Home Slider'
         verbose_name_plural = 'Home Sliders'
@@ -10,4 +12,19 @@ class HomeSliders(models.Model):
 
     def __str__(self):
         return self.slider_text
+
+
+# create fashion cards
+class FashionCards(models.Model):
+
+    class Meta:
+        verbose_name = 'Fashion Card'
+        verbose_name_plural = 'Fashion Cards'
+
+    image = models.ImageField()
+    phone_type = models.CharField(max_length=90)
+    price = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.phone_type
 
