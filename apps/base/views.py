3e809abe936d5d9671e866351e9ea038a4fd0f51
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-21 23:10:55 +0500

    create phone accessories context

--- apps/base/views.py
+++ apps/base/views.py
@@ -6,9 +6,11 @@ def base_home(request):
     sliders = HomeSliders.objects.all()
     fashion_cards = FashionCards.objects.all()
     phone_cover = PhoneBackCovers.objects.all()
+    phone_accessories = PhoneAccessories.objects.all()
     context = {
         "sliders": sliders,
         "fashion_cards": fashion_cards,
-        "phone_cover": phone_cover
+        "phone_cover": phone_cover,
+        "phone_accessories": phone_accessories
     }
     return render(request, 'base/pages/home.html', context)
