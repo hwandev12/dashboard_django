8abe756edd9270cddebdaecf43a351286a9c1607
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-21 21:30:48 +0500

    add media url to work properly

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include  # add this
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path("", include("apps.authentication.urls")),
     path("design/", include("apps.home.urls")),
     path("", include("apps.base.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
