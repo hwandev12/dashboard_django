c8f5265f585201b9b25af0e47eb172655195cc76
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-22 09:51:23 +0500

    add registration url

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from apps.base import views
+from .views import SignupView
+from django.contrib.auth.views import *
+
+app_name = 'phone'
 
 urlpatterns = [
     path('', views.base_home, name='base_home'),
+    path('signup/', SignupView.as_view(), name='registration')
 ]
