59e2e6052eb8a16c9e6bc7a70c985537376fae8e
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-22 09:51:40 +0500

    create sign up view

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import *
+from . import models
+from .forms import *
 from .models import *
 
 
+# base home function here
 def base_home(request):
     sliders = HomeSliders.objects.all()
     fashion_cards = FashionCards.objects.all()
@@ -14,3 +21,27 @@ def base_home(request):
         "phone_accessories": phone_accessories
     }
     return render(request, 'base/pages/home.html', context)
+
+
+# create sign up view
+class SignupView(CreateView):
+    template_name = 'base/registration/signup.html'
+    initial = {'key': 'value'}
+    form_class = RegisterForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
+
