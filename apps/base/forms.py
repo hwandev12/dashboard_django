1df2ca5b88e8265940213c993553e5b14bbf7c81
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-22 10:33:18 +0500

    create login form in forms file

--- apps/base/forms.py
+++ apps/base/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -57,3 +58,24 @@ class RegisterForm(UserCreationForm):
             'first_name', 'last_name', 'username', 'email', 'password1',
             'password2'
         ]
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
