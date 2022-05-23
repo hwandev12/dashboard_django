c32e98961a2c56152073705d6796ba7b655ad475
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-23 21:45:38 +0500

    create main profile update form

--- apps/base/forms.py
+++ apps/base/forms.py
@@ -1,8 +1,34 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from .models import Profile
+from apps.authentication.models import User
+
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(
+        attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 5
+    }))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
 
 
 # register form here
