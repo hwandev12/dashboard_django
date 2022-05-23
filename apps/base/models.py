aabae101d9431f03caeb0f3c04732ef0a167d96d
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-23 21:45:53 +0500

    create Profile model

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,9 +1,31 @@
+from PIL import Image
 from django.db import models
+from apps.authentication.models import User
+
+
+# make profile section here
+class Profile(models.Model):
+    user = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='redmi.jpg')
+    bio = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
+
+    def __str__(self):
+        return self.user.username
 
 
 # create home sliders model
 class HomeSliders(models.Model):
-
     class Meta:
         verbose_name = 'Home Slider'
         verbose_name_plural = 'Home Sliders'
