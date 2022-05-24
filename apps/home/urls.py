f780259994d254d70958b338b20bdc878c32effd
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-24 07:33:22 +0500

    create billing url

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -6,7 +6,7 @@ app_name = 'home'
 urlpatterns = [
     # The home page
     path('', views.index, name='home'),
-
+    path('billing/', views.billing, name='billing')
     # # Matches any html file
     # re_path(r'^.*\.*', views.pages, name='pages'),
 
