388f76916744efbba2406b6eccfa636df2f4a0e3
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-25 10:23:16 +0500

    create two additional models

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,4 +1,6 @@
+import phonenumbers
 from django.db import models
+from phonenumber_field.formfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
 
 
@@ -7,3 +9,8 @@ class User(AbstractUser):
     is_organiser = models.BooleanField(default=False)
     is_agent = models.BooleanField(default=False)
     is_user = models.BooleanField(default=True)
+    address = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    work_phone = models.IntegerField()
+
+
