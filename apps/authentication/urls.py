f58dbd7d9b2ebd10f34a7bdf70ac1a4083eba608
Husan <102412893+hwandev12@users.noreply.github.com>
2022-06-05 19:52:20 +0500

    password-reset-view

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import login_view, register_user
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordResetView
+)
+
+app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('password-reset-view/', PasswordResetView.as_view(), name='password-reset-view')
 ]
